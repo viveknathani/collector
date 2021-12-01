8e59a8f0a27ad43fcace9a5ed71a39380c06ae4d
viveknathani <viveknathani2402@gmail.com>
2021-12-01 18:31:12 +0530

    feat: setup job to collect

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,40 @@
-async function job() {
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+import { addForkCount, extractFromPR, handleError } from './util';
+import { PullRequest } from './interface';
+dotenv.config();
 
+// Collect PRs, extract information, sort by fork count
+async function job(user: string) {
+
+    const host = "https://api.github.com";
+    const endpoint = "/search/issues";
+    const query = `?q=is%3Apr+is%3Amerged+author%3A${user}`;
+
+    try {
+
+        const response = await fetch(host + endpoint + query);
+        const temp = await response.json();
+
+        const result: PullRequest[] = [];
+        for (let i = 0; i < temp.items.length; ++i) {
+            result.push(extractFromPR(temp.items[i]));
+        }
+        await addForkCount(result);
+        result.sort((x, y) => {
+            return y.repoForkCount - x.repoForkCount;
+        })
+
+        if (result.length < 5) {
+            return result;
+        }
+        return result.slice(0, 5);
+    } catch (err) {
+
+        handleError(err, 'job');
+    }
 }
+
+job(process.env.username || 'none')
+.then((result) => console.log(result))
+.catch((err) => console.log(err));
